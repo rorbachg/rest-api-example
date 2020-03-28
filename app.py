387fbb074f078e593d520a8b1f27a93eb9cc4e2b
Grzegorz Rorbach <rorbach.g@gmail.com>
2020-03-28 12:00:36 +0100

    move import to top of file

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models.user import UserModel
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
 
 
 app = Flask(__name__)
@@ -28,6 +29,5 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
