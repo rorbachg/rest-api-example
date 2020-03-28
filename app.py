fbdae11b8ef09e0ba08e898c61f629369e6881de
Grzegorz Rorbach <rorbach.g@gmail.com>
2020-03-28 16:15:51 +0100

    mobing init_app to the end

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
-db.init_app(app)
 @app.before_first_request
 def create_all_tables():
     db.create_all()
@@ -32,4 +31,4 @@ api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
-
+    db.init_app(app)
