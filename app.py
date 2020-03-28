a4a42dc741b48906983c4cc3db3ed58f737c422f
Grzegorz Rorbach <rorbach.g@gmail.com>
2020-03-28 16:20:42 +0100

    move db init to run.py.

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ from models.user import UserModel
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-
-@app.before_first_request
-def create_all_tables():
-    db.create_all()
-
 
 app.secret_key = '1234'
 api = Api(app)
@@ -31,4 +24,4 @@ api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
-    db.init_app(app)
+    
