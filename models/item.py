52c82e3d6a7c0f0381ccfda53634ca0342f67e0d
Grzegorz Rorbach <rorbach.g@gmail.com>
2020-03-30 10:09:14 +0200

    some small updates in code

--- models/item.py
+++ models/item.py
@@ -15,7 +15,12 @@ class ItemModel(db.Model):
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price, 'store_id': self.store_id}
+        return {
+            'id': self.id, 
+            'name': self.name, 
+            'price': self.price, 
+            'store_id': self.store_id
+            }
 
     def save_to_db(self):
         db.session.add(self)
@@ -28,3 +33,7 @@ class ItemModel(db.Model):
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
