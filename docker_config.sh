a00146c4ba183f7289f167f89709d408af358b83
Grzegorz Rorbach <rorbach.g@gmail.com>
2020-04-01 17:58:10 +0200

    nginx adn reordering

--- docker_config.sh
+++ docker_config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
-docker build . -t rorbachg/rest-api-example
-docker-compose up
+echo Running
+#docker build . -t rorbachg/rest-api-example
+#docker run -it rorbachg/rest-api-example
+#docker run -p 5000:5000 -w="/opt/source-code" rorbachg/rest-api-example
+docker-compose rm -fs
+#docker-compose up --build
+docker-compose -f docker-compose.prod.yml up --build 
